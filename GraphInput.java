442ded1a617d26fd25f3034f684e27bc7d4ee5f0
soe43 <soe433@gmail.com>
2017-01-09 12:35:05 -0500

    Began process of adding listeners to GraphInput.

--- GraphInput.java
+++ GraphInput.java
@@ -11,6 +11,8 @@ public class GraphInput extends JFrame implements ItemListener{
     final static String SCATTERPANEL = "Scatter Plot";
     final static String PIEPANEL = "Pie Graph";
     final static String HISTOPANEL = "Histogram";
+    final String[] sortOptions = {"No Sort","Ascending","Descending"};
+    public int[][] histoTable = new int[][];
     
     public GraphInput(){
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -24,84 +26,83 @@ public class GraphInput extends JFrame implements ItemListener{
 	cb.addItemListener(this);
 	comboBoxPane.add(cb);
 
-	JPanel card1 = new JPanel();
-	card1.setLayout(new BoxLayout(card1,BoxLayout.Y_AXIS));
-	card1.add(new JLabel("Input:"));
-	card1.add(new JTextField("INPUT RANGE", 5));
-	card1.add(new JCheckBox("Labels"));
-	JPanel c1buttons = new JPanel();
-	c1buttons.add(new JButton("Ok"));
-	c1buttons.add(new JButton("Cancel"));
-	card1.add(c1buttons);
+	JPanel lineCard = new JPanel();
+	lineCard.setLayout(new BoxLayout(lineCard,BoxLayout.Y_AXIS));
+	lineCard.add(new JLabel("Input:"));
+	lineCard.add(new JTextField("INPUT RANGE", 5));
+	lineCard.add(new JCheckBox("Labels"));
+	JPanel lineButtons = new JPanel();
+	lineButtons.add(new JButton("Ok"));
+	lineButtons.add(new JButton("Cancel"));
+	lineCard.add(lineButtons);
 
-	JPanel card2 = new JPanel();
-	card2.setLayout(new BoxLayout(card2,BoxLayout.Y_AXIS));
-	card2.add(new JLabel("Input:"));
-	card2.add(new JTextField("INPUT RANGE", 5));
-	card2.add(new JCheckBox("Labels"));
-	card2.add(new JLabel("Output"));
-	final String[] sortOptions = {"No Sort","Ascending","Descending"};
+	JPanel barCard = new JPanel();
+	barCard.setLayout(new BoxLayout(barCard,BoxLayout.Y_AXIS));
+	barCard.add(new JLabel("Input:"));
+	barCard.add(new JTextField("INPUT RANGE", 5));
+	barCard.add(new JCheckBox("Labels"));
+	barCard.add(new JLabel("Output"));
 	JComboBox cb1 = new JComboBox(sortOptions);
 	cb1.setEditable(false);
 	cb1.addItemListener(this);
-	card2.add(cb1);
-	JPanel c2buttons = new JPanel();
-	c2buttons.add(new JButton("Ok"));
-	c2buttons.add(new JButton("Cancel"));
-	card2.add(c2buttons);
+	barCard.add(cb1);
+	JPanel barButtons = new JPanel();
+	barButtons.add(new JButton("Ok"));
+	barButtons.add(new JButton("Cancel"));
+	barCard.add(barButtons);
 	
-	JPanel card3 = new JPanel();
-	card3.setLayout(new BoxLayout(card3,BoxLayout.Y_AXIS));
-	card3.add(new JLabel("Input:"));
-	card3.add(new JTextField("INPUT RANGE",5));
-	card3.add(new JTextField("INPUT BIN RANGE",5));
-	card3.add(new JCheckBox("Labels"));
-	card3.add(new JLabel("Output:"));
-	card3.add(new JTextField("OUTPUT RANGE"));
-	card3.add(new JCheckBox("Table"));
+	JPanel scatterCard = new JPanel();
+	scatterCard.setLayout(new BoxLayout(scatterCard,BoxLayout.Y_AXIS));
+	scatterCard.add(new JLabel("Input:"));
+	scatterCard.add(new JTextField("INPUT RANGE",5));
+	scatterCard.add(new JTextField("INPUT BIN RANGE",5));
+	scatterCard.add(new JCheckBox("Labels"));
+	scatterCard.add(new JLabel("Output:"));
+	scatterCard.add(new JTextField("OUTPUT RANGE"));
+	scatterCard.add(new JCheckBox("Table"));
 	JComboBox cb2 = new JComboBox(sortOptions);
 	cb2.setEditable(false);
 	cb2.addItemListener(this);
-	card3.add(cb2);
-	JPanel c3buttons = new JPanel();
-	c3buttons.add(new JButton("Ok"));
-	c3buttons.add(new JButton("Cancel"));
-	card3.add(c3buttons);
+	scatterCard.add(cb2);
+	JPanel scatterButtons = new JPanel();
+	scatterButtons.add(new JButton("Ok"));
+	scatterButtons.add(new JButton("Cancel"));
+	scatterCard.add(scatterButtons);
 	
-	JPanel card4 = new JPanel();
-	card4.setLayout(new BoxLayout(card4,BoxLayout.Y_AXIS));
-	card4.add(new JLabel("Input:"));
-	card4.add(new JTextField("INPUT RANGE", 5));
-	card4.add(new JCheckBox("Labels"));
-	JPanel c4buttons = new JPanel();
-	c4buttons.add(new JButton("Ok"));
-	c4buttons.add(new JButton("Cancel"));
-	card4.add(c4buttons);
+	JPanel pieCard = new JPanel();
+	pieCard.setLayout(new BoxLayout(pieCard,BoxLayout.Y_AXIS));
+	pieCard.add(new JLabel("Input:"));
+	pieCard.add(new JTextField("INPUT RANGE", 5));
+	pieCard.add(new JCheckBox("Labels"));
+	JPanel pieButtons = new JPanel();
+	pieButtons.add(new JButton("Ok"));
+	pieButtons.add(new JButton("Cancel"));
+	pieCard.add(pieButtons);
 	
-	JPanel histocard = new JPanel();
-	histocard.setLayout(new BoxLayout(histocard,BoxLayout.Y_AXIS));
-	histocard.add(new JLabel("Input:"));
-	histocard.add(new JTextField("INPUT RANGE", 5));
-	histocard.add(new JTextField("INPUT BIN RANGE",5));
-	histocard.add(new JCheckBox("Labels"));
-	histocard.add(new JLabel("Output:"));
-	histocard.add(new JTextField("OUTPUT RANGE"));
-	histocard.add(new JCheckBox("Chart"));
+	JPanel histoCard = new JPanel();
+	histoCard.setLayout(new BoxLayout(histoCard,BoxLayout.Y_AXIS));
+	histoCard.add(new JLabel("Input:"));
+	histoCard.add(new JTextField("INPUT RANGE", 5));
+	histoCard.add(new JTextField("INPUT BIN RANGE",5));
+	histoCard.add(new JCheckBox("Labels"));
+	histoCard.add(new JLabel("Output:"));
+	histoCard.add(new JTextField("OUTPUT RANGE"));
+	histoCard.add(new JCheckBox("Chart"));
 	JComboBox cb3 = new JComboBox(sortOptions);
 	cb3.setEditable(false);
 	cb3.addItemListener(this);
-	histocard.add(cb3);		
-	JPanel histobuttons = new JPanel();
-	histobuttons.add(new JButton("Ok"));
-	histobuttons.add(new JButton("Cancel"));
-	histocard.add(histobuttons);
+	histoCard.add(cb3);		
+	JPanel histoButtons = new JPanel();
+	histoButtons.add(new JButton("Ok"));
+	histoButtons.add(new JButton("Cancel"));
+	histoCard.add(histoButtons);
 	
 	cards = new JPanel(new CardLayout());
-	cards.add(card1, LINEPANEL);
-	cards.add(card2, BARPANEL);
-	cards.add(card3, SCATTERPANEL);
-	cards.add(card4, PIEPANEL);
-	cards.add(histocard, HISTOPANEL);
+	cards.add(lineCard, LINEPANEL);
+	cards.add(barCard, BARPANEL);
+	cards.add(scatterCard, SCATTERPANEL);
+	cards.add(pieCard, PIEPANEL);
+	cards.add(histoCard, HISTOPANEL);
 	
 	pane = this.getContentPane();
 	pane.add(comboBoxPane, BorderLayout.PAGE_START);
@@ -113,6 +114,9 @@ public class GraphInput extends JFrame implements ItemListener{
 	c1.show(cards, (String)e.getItem());
     }
 
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+    
     public static void main(String[]args){
 	GraphInput g = new GraphInput();
 	g.setVisible(true);
