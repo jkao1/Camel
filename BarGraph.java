e3029ed1850ad08d134fb4ea64b04cdc14bfc47f
soe43 <soe433@gmail.com>
2017-01-17 02:06:37 -0500

    finished up Bargraph. Waiting for integration

--- BarGraph.java
+++ BarGraph.java
@@ -1,89 +1,109 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 
-/** 
- *Reference taken from:
- *@author Java2s
- *@url http://www.java2s.com/Code/Java/2D-Graphics-GUI/Simplebarchart.htm
- */
+public class BarGraph extends JPanel {
 
-public class BarGraph extends JPanel{
-    private double[] values;
-    private String[] labels;
-    private String title;
+    private JFrame frame;
     
-    public BarGraph(double[] vals, String[] l, String t){
-	values = vals;
-	labels = null;
-	title = "";
+    private List<Integer> values;
+    private List<String> labels;
+    private String title; 
+
+    public BarGraph( List<Integer> v, List<String> l, String t) {	
+	values = v;
+	labels = l;
+	title = t;
     }
 
-    protected void paintComponent(Graphics g){
+    public void paintComponent( Graphics g )
+    {
 	super.paintComponent(g);
-	if(values.length == 0 || values == null){
+
+	if (values == null || values.size() == 0) {
 	    return;
 	}
 
-	double min = 0;
-	double max = 0;
-
-	//find min and max values of the values to chart
-	for (int i = 0; i > values.length; i++){
-	    if (min > values[i])
-		min = values[i];
-	    if (max < values[i])
-		max = values[i];
+	double minValue = 0;
+	double maxValue = 0;
+	for (int i = 0; i < values.size(); i++) {
+	    if (minValue > values.get(i)) {
+		minValue = values.get(i);
+	    }
+	    if (maxValue < values.get(i)) {		
+		maxValue = values.get(i);
+	    }
 	}
-	
-	Dimension d = getSize();
-	int windowWidth = d.width;
-	int windowHeight = d.height;
-	int barWidth = windowWidth / values.length;
 
-	//Fonts for strings used later for spacing
-	Font titleFont = new Font("SansSerif", Font.BOLD, 20);
+	Dimension dim = getSize();
+	int clientWidth = dim.width;
+	int clientHeight = dim.height;
+	int barWidth = clientWidth / values.size();
+
+        Font titleFont = new Font("SansSerif", Font.BOLD, 20);
 	FontMetrics titleFontMetrics = g.getFontMetrics(titleFont);
 	Font labelFont = new Font("SansSerif", Font.PLAIN, 10);
 	FontMetrics labelFontMetrics = g.getFontMetrics(labelFont);
 
-	//Creating the space which the graph can use
+	int titleWidth = titleFontMetrics.stringWidth(title);
+	int y = titleFontMetrics.getAscent();
+	int x = (clientWidth - titleWidth) / 2;
+	g.setFont(titleFont);
+	g.drawString(title, x, y);
+
 	int top = titleFontMetrics.getHeight();
 	int bottom = labelFontMetrics.getHeight();
-	if (max == min)
+	if (maxValue == minValue) {
 	    return;
-	double scale = (windowHeight - top - bottom) / (max - min);
+	}
+	double scale = (clientHeight - top - bottom) / (maxValue - minValue);
+	y = clientHeight - labelFontMetrics.getDescent();
 	g.setFont(labelFont);
 
-	//filling in the rectangles for the bars
-	for (int i = 0; i < values.length; i++) {
+	for (int i = 0; i < values.size(); i++) {
 	    int valueX = i * barWidth + 1;
 	    int valueY = top;
-	    int height = (int) (values[i] * scale);
-	    if (values[i] >= 0)
-		valueY += (int) ((max- values[i]) * scale);
+	    int height = (int) (values.get(i) * scale);
+	    if (values.get(i) >= 0)
+		valueY += (int) ((maxValue - values.get(i)) * scale);
 	    else {
-		valueY += (int) (max * scale);
+		valueY += (int) (maxValue * scale);
 		height = -height;
 	    }
-	    
-	    g.setColor(Color.black);
+
+	    g.setColor(Color.red);
 	    g.fillRect(valueX, valueY, barWidth - 2, height);
 	    g.setColor(Color.black);
 	    g.drawRect(valueX, valueY, barWidth - 2, height);
+	    int labelWidth = labelFontMetrics.stringWidth(labels.get(i));
+	    x = i * barWidth + (barWidth - labelWidth) / 2;
+	    g.drawString(labels.get(i) + ": " + values.get(i), x, y);
 	}
     }
 
+    
+     private static void createAndShowGui(){
+	List<Integer> scores = new ArrayList<Integer>();
+	List<String> labels = new ArrayList<String>();
+	String graph = "Graph";
+	//insert points into score here
+	Histogram mainPanel = new BarGraph(scores, labels, graph);
 
-    public static void main(String[]args){
-	JFrame frame = new JFrame();
-	double[] values = new double[3];
-	values[0] = 1;
-	values[1] = 2;
-        values[2] = 3;
-	frame.setSize(800,600);
+	JFrame frame = new JFrame("Line Graph");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().add(new BarGraph(values,null,""));
+	frame.getContentPane().add(mainPanel);
+	frame.pack();
+	frame.setLocationByPlatform(true);
 	frame.setVisible(true);
     }
 }
-				   
