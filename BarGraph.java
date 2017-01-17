f04aded309ff00f8c3321a64c56d83ae7b70f78c
soe43 <soe433@gmail.com>
2017-01-17 01:37:34 -0500

    Developed BarGraph further

--- BarGraph.java
+++ BarGraph.java
@@ -1,11 +1,21 @@
 import java.awt.*;
 import javax.swing.*;
 
+/** 
+ *Reference taken from:
+ *@author Java2s
+ *@url http://www.java2s.com/Code/Java/2D-Graphics-GUI/Simplebarchart.htm
+ */
+
 public class BarGraph extends JPanel{
     private double[] values;
-
-    public BarGraph(double[] vals){
+    private String[] labels;
+    private String title;
+    
+    public BarGraph(double[] vals, String[] l, String t){
 	values = vals;
+	labels = null;
+	title = "";
     }
 
     protected void paintComponent(Graphics g){
@@ -24,29 +34,45 @@ public class BarGraph extends JPanel{
 	    if (max < values[i])
 		max = values[i];
 	}
-
-	Dimension window = getSize();
-	int windowWidth = window.width;
-	int windowHeight = window.height;
-	int barsWidth = windowWidth/values.length;
-
-	for(int i = 0; i<values.length; i++){
-	    int Xcoord = i * barsWidth + 1;
-	    int Ycoord = windowHeight;
-	    int barHeight = (int)values[i];
-		if(values[i] >= 0){
-		    Ycoord += (int)(max - values[i]);
-		}
-		else{
-		    Ycoord += max;
-		    barHeight = -barHeight;
-		}
 	
+	Dimension d = getSize();
+	int windowWidth = d.width;
+	int windowHeight = d.height;
+	int barWidth = windowWidth / values.length;
 
-	g.setColor(Color.black);
-	g.fillRect(Xcoord, Ycoord, barsWidth - 2, barHeight);
+	//Fonts for strings used later for spacing
+	Font titleFont = new Font("SansSerif", Font.BOLD, 20);
+	FontMetrics titleFontMetrics = g.getFontMetrics(titleFont);
+	Font labelFont = new Font("SansSerif", Font.PLAIN, 10);
+	FontMetrics labelFontMetrics = g.getFontMetrics(labelFont);
+
+	//Creating the space which the graph can use
+	int top = titleFontMetrics.getHeight();
+	int bottom = labelFontMetrics.getHeight();
+	if (max == min)
+	    return;
+	double scale = (windowHeight - top - bottom) / (max - min);
+	g.setFont(labelFont);
+
+	//filling in the rectangles for the bars
+	for (int i = 0; i < values.length; i++) {
+	    int valueX = i * barWidth + 1;
+	    int valueY = top;
+	    int height = (int) (values[i] * scale);
+	    if (values[i] >= 0)
+		valueY += (int) ((max- values[i]) * scale);
+	    else {
+		valueY += (int) (max * scale);
+		height = -height;
+	    }
+	    
+	    g.setColor(Color.black);
+	    g.fillRect(valueX, valueY, barWidth - 2, height);
+	    g.setColor(Color.black);
+	    g.drawRect(valueX, valueY, barWidth - 2, height);
 	}
     }
+
 
     public static void main(String[]args){
 	JFrame frame = new JFrame();
@@ -56,7 +82,7 @@ public class BarGraph extends JPanel{
         values[2] = 3;
 	frame.setSize(800,600);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().add(new BarGraph(values));
+	frame.getContentPane().add(new BarGraph(values,null,""));
 	frame.setVisible(true);
     }
 }
